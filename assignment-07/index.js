f7f5ea515f1ec41acacad2a99b5cd82ced7b65a5
vedantpatel-simform <vedant.patel@simformsolutions.com>
2025-04-02 18:51:58 +0530

    add: assignment 7

---
+++ assignment-07/index.js
@@ -0,0 +1,78 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
+const querystring = require("querystring");
+
+const port = 3000;
+
+// Create the server
+http
+  .createServer((req, res) => {
+    // Redirect from `/` to `/register`
+    if (req.method === "GET" && req.url === "/") {
+      res.writeHead(302, { Location: "/register" });
+      res.end();
+    }
+
+    // Route for showing the registration form
+    else if (req.method === "GET" && req.url === "/register") {
+      const filePath = path.join(__dirname, "views", "form.ejs");
+
+      fs.readFile(filePath, "utf-8", (err, template) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Error reading form template.");
+          return;
+        }
+
+        // Render the registration form template
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(template);
+      });
+    }
+
+    // Handle form submission
+    else if (req.method === "POST" && req.url === "/submit") {
+      let body = "";
+      req.on("data", (chunk) => {
+        console.log("data come");
+        console.log("DATA = ", chunk); // data come as buffer
+        body += chunk; // js automatically type coersion to string
+      });
+
+      req.on("end", () => {
+        console.log(body);
+        const formData = querystring.parse(body); // Convert the query string to an object
+
+        const data = {
+          name: formData.name,
+          email: formData.email,
+        };
+
+        const filePath = path.join(__dirname, "views", "submitted.ejs");
+
+        fs.readFile(filePath, "utf-8", (err, template) => {
+          if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error reading submitted template.");
+            return;
+          }
+
+          // display the submitted data on the submitted page
+          const html = ejs.render(template, data);
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(html);
+        });
+      });
+    }
+
+    // if the user try to access other routes
+    else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page not found");
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
